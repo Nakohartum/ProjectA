48ed842f2ff6d247a45b925d680c40d2229b1dd1
Nakohartum <artemofgta@gmail.com>
2023-04-18 17:41:53 +0500

    Домашнее задание 4

---
+++ Assets/_Root/Scripts/Authorization/SignIn.cs
@@ -0,0 +1,41 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Root.Scripts.Authorization
+{
+    public class SignIn : AuthorizationWindow
+    {
+        protected override void InitializeComponents()
+        {
+            base.InitializeComponents();
+            _acceptButton.onClick.AddListener(LoginUser);
+        }
+
+        private void LoginUser()
+        {
+            StartWaiting();
+            PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest()
+            {
+                Username = _username,
+                Password = _password
+            }, LoginSuccessful, LoginError);
+        }
+
+        private void LoginError(PlayFabError error)
+        {
+            var errorMessage = error.GenerateErrorReport();
+            Debug.Log($"Error: {errorMessage}");
+            _isLoading = false;
+        }
+
+        private void LoginSuccessful(LoginResult result)
+        {
+            Debug.Log($"Player {result.PlayFabId} successfully logged in");
+            _isLoading = false;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
